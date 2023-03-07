26e9734eea348efda282adce00e78e874c2953b0
katieschaum <83676554+katieschaum@users.noreply.github.com>
2023-03-07 12:37:28 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import DrugTable from "./components/DrugTable";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const router = createBrowserRouter([
+  {
+    path:"/", element:<App />
+  },
+  {
+    path:"/drugs/:animal", element:<DrugTable />
+  }
+])
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
