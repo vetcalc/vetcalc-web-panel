32e724ff0a60e24b1d4beab7a354366417464f46
foreman23 <foremanb@oregonstate.edu>
2023-03-12 17:55:31 -0700

    dropdown retrieves animals from DB

--- src/components/animalList.js
+++ src/components/animalList.js
@@ -1,17 +1,41 @@
-import _ from 'lodash'
-import React, { Component } from 'react'
-import { Dropdown } from 'semantic-ui-react'
+import {React, useState} from "react";
+import { Component } from "react";
+import { Dropdown, Form, Input } from "semantic-ui-react";
+import api from "../services/api";
+import axios from 'axios';
+import { getValue } from "@testing-library/user-event/dist/utils";
 
 // Creates the dropdown component on the homepage for the user to select which animal to view.
-const animals = ["", "Camelid", "Cat", "Cattle", "Dog", "Equine", "Goat/Sheep", "Swine" ];
-const animalOptions = _.map(animals, (animal, index) => ({
-  key: animals[index],
-  text: animal,
-  value: animals[index],
-}))
+// const animals = ["", "Camelid", "Cat", "Cattle", "Dog", "Equine", "Goat/Sheep", "Swine",];
+// const animalOptions = _.map(animals, (animal, index) => ({
+//   key: animals[index],
+//   text: animal,
+//   value: animals[index],
+// }))
+
+
 
 export default class DropdownAnimalSearchQuery extends Component {
   state = { searchQuery: '' }
+
+// Retrieve data from DB using Axios
+  componentDidMount() {
+    // Get animal information
+    api.get('/animals')
+    .then(response => {
+      const animalOptions = response.data.map(animal => ({
+        key: animal.animal_id,
+        text: animal.name,
+        value: animal.name
+      }
+      ));
+      this.setState({animalOptions});
+    })
+    .catch(error => {
+      console.error("Error: cannot receive animal data from DB")
+    })
+  }
+
 
   handleChange = (e, { searchQuery, value }) =>{
     if (value === 'Goat/Sheep') {
@@ -31,7 +55,7 @@ export default class DropdownAnimalSearchQuery extends Component {
         fluid
         onChange={this.handleChange}
         onSearchChange={this.handleSearchChange}
-        options={animalOptions}
+        options={this.state.animalOptions}
         placeholder='Animal'
         search
         searchQuery={searchQuery}
--- src/components/vitalsList.js
+++ src/components/vitalsList.js
@@ -2,31 +2,32 @@ import React from "react";
 import { Table } from "semantic-ui-react";
 import api from "../services/api";
 
+
 // Creates the table with the animals expected biometrics
 
 const VitalsCard = (props) => {
-    // function get(){
-    //     api.get('/animals', {
-    //         //drug_id: 161,
-    //         name: this.state.name,
-    //         temperature_low: this.state.temperature_low,
-    //         temperature_high: this.state.temperature_high,
-    //         heart_rate_low: this.state.heart_rate_low,
-    //         heart_rate_high: this.state.heart_rate_high,
-    //         respiratory_rate_low: this.state.respiratory_rate_low,
-    //         respiratory_rate_high: this.state.respiratory_rate_high,
-    //         Authentication: "simian_army_makes_chaos"
-    //       })
-    //       .then(function (response) {
-    //         console.log(response);
-    //       })
-    //       .catch(function (error) {
-    //         console.log(error);
-    //       });
+    function get(){
+        api.get('/animals', {
+            //drug_id: 161,
+            name: this.state.name,
+            temperature_low: this.state.temperature_low,
+            temperature_high: this.state.temperature_high,
+            heart_rate_low: this.state.heart_rate_low,
+            heart_rate_high: this.state.heart_rate_high,
+            respiratory_rate_low: this.state.respiratory_rate_low,
+            respiratory_rate_high: this.state.respiratory_rate_high,
+            Authentication: "simian_army_makes_chaos"
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
 
           
-    //     return;
-    // }
+        return;
+    }
     const { temperature_low, temperature_high, 
         heart_rate_low, heart_rate_high, 
         respiratory_rate_low, respiratory_rate_high} = "5";
